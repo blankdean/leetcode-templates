11eb8910d6bb9afaf2de88fffc8d2a8dd081f0bc
blankdean <blankdean@gmail.com>
2022-11-18 01:49:15 -0500

    update templates

--- templates/trie.py
+++ templates/trie.py
@@ -1,41 +1,33 @@
-class TrieNode:
-    def __init__(self):
-        self.isWord = False
-        self.nodes = {}
-            
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.is_word = False
+        self.children = {}
 
-    # Inserts a word into the trie.
     def insert(self, word: str) -> None:
-        cur = self.root
-        
-        for c in word:
-            if c not in cur.nodes:
-                cur.nodes[c] = TrieNode()
-            cur = cur.nodes[c]
-            
-        cur.isWord = True
+        curr = self
 
-    # Returns if the word is in the trie
-    def search(self, word: str) -> bool:
-        cur = self.root
-        
         for c in word:
-            if c not in cur.nodes:
+            if c not in curr.children:
+                curr.children[c] = Trie()
+            curr = curr.children[c]
+        curr.is_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self
+
+        for c in word:
+            if c not in curr.children:
                 return False
-            cur = cur.nodes[c]
-            
-        return cur.isWord
-    # Returns if there is any word in the trie 
-    # that starts with the given prefix. */
+            curr = curr.children[c]
+
+        return curr.is_word
+
     def startsWith(self, prefix: str) -> bool:
-        cur = self.root
-        
+        curr = self
+
         for c in prefix:
-            if c not in cur.nodes:
+            if c not in curr.children:
                 return False
-            cur = cur.nodes[c]
-            
+            curr = curr.children[c]
         return True
+        
